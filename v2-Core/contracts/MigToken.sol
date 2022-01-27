d973f7186ede18144585756926af5734ffdc601a
Mayank <mayanksae@gmail.com>
2022-01-27 15:06:57 -0500

    mytokens

--- v2-Core/contracts/MigToken.sol
+++ v2-Core/contracts/MigToken.sol
@@ -5,7 +5,6 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract MigToken is ERC20Detailed, ERC20 {    
   constructor() ERC20Detailed('Mig Token', 'Mig', 18) public {
-
   } 
 }
 
--- v2-Core/contracts/SukhoiToken.sol
+++ v2-Core/contracts/SukhoiToken.sol
@@ -5,6 +5,5 @@ import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract SukhoiToken is ERC20Detailed, ERC20 {
   constructor() ERC20Detailed('Sukhoi Token', 'Sukhoi', 18) public {  
-
   } 
 }
