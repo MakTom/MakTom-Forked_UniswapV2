7c3cd5574f896a7d0477188eee77420c9c9590d9
Mayank <mayanksae@gmail.com>
2022-01-27 15:00:16 -0500

    inital commit

---
+++ v2-Core/contracts/MigToken.sol
@@ -0,0 +1,12 @@
+pragma solidity =0.5.16;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract MigToken is ERC20Detailed, ERC20 {    
+  constructor() ERC20Detailed('Mig Token', 'Mig', 18) public {
+
+  } 
+}
+
+ 
